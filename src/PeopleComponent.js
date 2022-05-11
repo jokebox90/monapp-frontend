0783cadf11726456c5d0dcff64d4a7da86599c26
Jonathan Vagnier <adm.jvagnier@gmail.com>
2022-05-11 02:12:03 +0200

    Corrige la récupération et le rendu de données

--- src/PeopleComponent.js
+++ src/PeopleComponent.js
@@ -1,76 +1,71 @@
 // src/PeopleComponent.js
 
 import _ from "lodash";
-import { createRef, forwardRef, Fragment, useEffect, useState } from "react";
+import { createRef, Fragment, useEffect, useState } from "react";
 import { addPeople, getPeople } from "./PeopleData";
 
-const AddPeopleComponent = forwardRef((props, ref) => {
+const AddPeopleComponent = (props) => {
+  const addPeopleRef = createRef();
+
+  const handleAddPeople = (e) => {
+    e.preventDefault();
+    addPeople(addPeopleRef.current.value)
+      .then(props.addPeopleCallback);
+  }
+
   return (
     <Fragment>
-      <label>{props.hello}</label>
-      <input type="text" className="input is-large" name="add_people" ref={ref} />
+      <div className="field">
+        <div className="control">
+          <label className="label">Inviter un VIP</label>
+          <input type="text" className="input is-large" name="add_people" ref={addPeopleRef} />
+        </div>
+      </div>
+      <div className="field">
+        <div className="control">
+          <button onClick={handleAddPeople} className="button">
+            Ajouter à la liste
+          </button>
+        </div>
+      </div>
     </Fragment>
   );
-});
+};
 
 const PeopleComponent = () => {
-  // - Objet pour transporter l'état de
-  //   l'ensemble composant
-  const initPeopleState = {
-    // - Sentinelle pour éviter le chargement
-    //   depuis l'API à l'infini
-    isLoaded: false,
-    // - Conteneur qui reçois les données et
-    //   permet de les utiliser dans le composant
-    data: null,
-  };
 
   // Créé l'état principal du composant à partir de l'objet init
-  const [peopleState, setPeopleState] = useState(initPeopleState);
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState(null);
 
-  // - Effectue une opération en parallèle, une
-  //   boucle "side effect") pour charger les
-  //   données depuis l'API
   useEffect(() => {
-    if (!peopleState.isLoaded) {
-      // - Créé une copie de l'état en cours pour
-      //   une nouvelle configuration du composant
-      const newPeopleState = { ...peopleState };
-      newPeopleState.data = getPeople();
-      newPeopleState.isLoaded = true;
-      setPeopleState(newPeopleState)
+    if (loading) {
+      const newData = getPeople()
+      .then((peoples) => {
+        console.log(newData);
+        setLoading(false);
+        setData(peoples);
+      });
     }
-  }, [peopleState]);
+  }, [data, loading]);
 
   // - N'affiche qu'un message tant que la boucle
   //   n'a pas pu charger les données
   //   API inaccessible ??
-  if (!peopleState.isLoaded) return <p>Liste des personnes en cours de chargement...</p>;
+  if (loading) return <p>Liste de personnes en cours de chargement...</p>;
+  if (!data) return <p>Erreur de chargement !</p>;
 
-  const addPeopleRef = createRef();
-  const handleAddPeople = (e) => {
-    e.preventDefault();
-    alert(addPeopleRef.current.value);
-    addPeople(addPeopleRef.current.value);
-  }
-
-  console.log(peopleState);
+  // console.log(data);
   return (
     <Fragment>
-      <AddPeopleComponent ref={addPeopleRef} hello="world" />
-      <button onClick={handleAddPeople} className="button">
-        Ajouter un VIP
-      </button>
-
-      <ol>
-        {peopleState.data && _.map(peopleState.data, (people, index) => {
-          return (
-            <li key={index}>
-              {people}
-            </li>
-          );
-        })}
-      </ol>
+      <AddPeopleComponent addPeopleCallback={() => setLoading(true)} />
+      {_.map(data, (people, index) => {
+        return (
+          <button className="button" key={index}>
+            {people}
+          </button>
+        );
+      })}
     </Fragment>
   );
 }
