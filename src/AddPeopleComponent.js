e13db5fb491f70df19d10ae843f10f3fad258442
Jonathan Vagnier <adm.jvagnier@gmail.com>
2022-05-13 14:57:54 +0200

    Fait manger des toast à React

--- src/AddPeopleComponent.js
+++ src/AddPeopleComponent.js
@@ -1,33 +1,62 @@
 // src/PeopleComponent.js
 
 import _ from "lodash";
-import { createRef, Fragment,  } from "react";
+import { createRef, Fragment, useEffect, useState,  } from "react";
 import { addPeople } from "./PeopleData";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const AddPeopleComponent = (props) => {
-  const addPeopleRef = createRef();
+  const initState = {
+    message: null,
+  };
+  const [message, setMessage] = useState(null);
+
+  const inputRef = createRef();
+  const buttonRef = createRef();
 
   const handleAddPeople = (e) => {
     e.preventDefault();
-    addPeople(addPeopleRef.current.value)
-      .then(props.addPeopleCallback);
+
+    const value = inputRef.current.value;
+
+    if (!value || value === "") {
+      toast("Oups ! Impossible d'enregistrer une valeure vide.", {
+        onOpen: () => {
+          inputRef.current.className = "input is-small is-rounded is-danger has-background-danger-light";
+          buttonRef.current.className = "button is-small is-rounded is-danger";
+        },
+        onClose: () => {
+          inputRef.current.className = "input is-small is-rounded";
+          buttonRef.current.className = "button is-small is-rounded is-success";
+        },
+      });
+    }
+    else {
+      addPeople(inputRef.current.value)
+        .then(props.addPeopleCallback);
+    };
   }
 
   return (
     <Fragment>
-      <div className="field">
+      <div className="field has-addons is-flex is-justify-content-center">
         <div className="control">
-          <label className="label">Inviter un VIP</label>
-          <input type="text" className="input is-large" name="add_people" ref={addPeopleRef} />
+          <input
+            type="text"
+            className="input is-small is-rounded"
+            name="add_people"
+            placeholder="Veuillez taper un nom ici..."
+            ref={inputRef}
+          />
         </div>
-      </div>
-      <div className="field">
         <div className="control">
-          <button onClick={handleAddPeople} className="button">
-            Ajouter à la liste
+          <button className="button is-small is-rounded is-success" onClick={handleAddPeople} ref={buttonRef}>
+            Inviter
           </button>
         </div>
       </div>
+      <ToastContainer />
     </Fragment>
   );
 };
